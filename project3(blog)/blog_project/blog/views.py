04c54047af19e3c3a6d6670b4aebaa8f0a17d9fc
flamcom27 <27flamcom06@gmail.com>
2022-04-13 22:22:08 +0300

    "PostCreateView" is created

--- project3(blog)/blog_project/blog/views.py
+++ project3(blog)/blog_project/blog/views.py
@@ -1,4 +1,4 @@
-from django.views.generic import ListView, DetailView
+from django.views.generic import ListView, DetailView, CreateView
 from .models import Post
 
 # Create your views here.
@@ -11,3 +11,7 @@ class BlogDetailView(DetailView):
     model = Post
     template_name = 'post_detail.html'
     
+class BlogCreateView(CreateView):
+    model = Post
+    template_name = 'post_new.html'
+    fields = ['title', 'author', 'body']
